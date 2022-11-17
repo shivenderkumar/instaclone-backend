cc34576861a36632be098eb1da941ee478ae9865
shiven.vrm@gmail.com <shiven.vrm@gmail.com>
2022-11-18 03:23:40 +0530

    init instaclone backend

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8080;
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.end("Welcome to instaclone backend");
+})
+
+app.listen(port, ()=> console.log(`Server started listening on port ${port}`))
+module.exports =app;
