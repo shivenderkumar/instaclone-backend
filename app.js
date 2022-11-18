195826f22885c66ec4248dd37ed59b703e62db61
shiven.vrm@gmail.com <shiven.vrm@gmail.com>
2022-11-19 01:52:33 +0530

    implement - get posts api

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const port = 8080;
 
 const app = express();
 
+app.get("/posts",(req,res)=>{
+    try{
+        //get all posts from mongodb
+        const posts = []; //getAllPosts()
+        res.status(200).json(posts);
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
+
 app.get("/", (req,res)=>{
     res.end("Welcome to instaclone backend");
 })
