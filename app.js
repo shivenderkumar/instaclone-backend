a58a33254c72f15dfa653a43a4ba25804c0ecab0
shiven.vrm@gmail.com <shiven.vrm@gmail.com>
2022-11-19 02:22:57 +0530

    implement routes folder, get and post methods without mongodb

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const port = 8080;
+const mongoose = require('mongoose');
 
 const app = express();
-
-app.get("/posts",(req,res)=>{
-    try{
-        //get all posts from mongodb
-        const posts = []; //getAllPosts()
-        res.status(200).json(posts);
-    }catch(err){
-        res.status(400).json(err)
-    }
-})
 
 app.get("/", (req,res)=>{
     res.end("Welcome to instaclone backend");
